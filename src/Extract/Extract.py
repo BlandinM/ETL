d9248cf24d01b8cf3d2635e0b9bcb787894ddcd8
Blandin <almendarezbictor@gmail.com>
2025-07-23 23:36:31 -0600

    manejo de datos existentes

--- src/Extract/Extract.py
+++ src/Extract/Extract.py
@@ -1,30 +1,43 @@
 from Conection.ConectionBD import ConectionBD
 from Conection.Conection import Conection
 from Transform.Transform import Transform
-import pandas as pd
 
-class Extract : 
+class Extract:
+    """
+    Clase encargada de realizar la extracción de datos desde la base OLTP 
+    y enviarlos a tranformación.
+    """
 
-    def __init__(self,database,databaseOLAP,query,tableOLAP,conexion):
+    def __init__(self, database, databaseOLAP, query, tableOLAP, conexion):
+        """
+        Inicializa los parámetros necesarios para el proceso de extracción.
+
+        Args:
+            database (str): Nombre de la base de datos OLTP (origen).
+            databaseOLAP (str): Nombre de la base de datos OLAP (destino).
+            query (str): Consulta SQL para extraer los datos.
+            tableOLAP (str): Nombre de la tabla destino en la base OLAP.
+            conexion (Conection): Objeto de conexión para interactuar con las bases de datos.
+        """
         self.queryOLTP = query
         self.tableOLAP = tableOLAP
         self.databaseOLTP = database
         self.conexion = conexion
         self.dbOLAP = databaseOLAP
-        
 
     def getData(self):
-      
-        try:
-           conexion =self.conexion.getengine(self.dbOLAP).connect()
-           
-           df = self.conexion.getDataTables(self.databaseOLTP,self.queryOLTP)
-           print(len(df.columns.to_list()))
-           transform = Transform()
-           transform.processData(df,self.conexion,self.dbOLAP,self.tableOLAP)
+        """
+        Realiza la extracción de datos desde la base OLTP utilizando la consulta proporcionada,
+         y llama al proceso de transformación .
         
-          
-           
-       
-        except  Exception as e:
-           print(e)
+        """
+        try:
+            conexion = self.conexion.getengine(self.dbOLAP).connect()
+
+            df = self.conexion.getDataTables(self.databaseOLTP, self.queryOLTP)
+            print(len(df.columns.to_list()))
+            transform = Transform()
+            transform.processData(df, self.conexion, self.dbOLAP, self.tableOLAP)
+
+        except Exception as e:
+            print(e)
